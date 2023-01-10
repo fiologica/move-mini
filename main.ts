2e373e52c1623715359cdc20d1a315c28e8a0517
fiologica <39060769+fiologica@users.noreply.github.com>
2023-01-10 22:23:00 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,21 @@
+enum RadioMessage {
+    message1 = 49434
+}
+radio.onReceivedNumber(function (receivedNumber) {
+    item = receivedNumber
+})
+let item = 0
+radio.setGroup(44)
+basic.showIcon(IconNames.Happy)
+servos.P0.setRange(0, 360)
+basic.forever(function () {
+    if (item == 1) {
+        pins.servoWritePin(AnalogPin.P1, 180)
+        pins.servoWritePin(AnalogPin.P2, 0)
+    } else if (item == 2) {
+        pins.servoWritePin(AnalogPin.P1, 0)
+        pins.servoWritePin(AnalogPin.P2, 180)
+    } else if (item == 3) {
+        kitronik_servo_lite.stop()
+    }
+})
