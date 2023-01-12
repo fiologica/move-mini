30b680b8e6e37dfb1959611f05837f81c6f81b5e
fiologica <39060769+fiologica@users.noreply.github.com>
2023-01-12 17:05:43 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,11 +4,7 @@ enum RadioMessage {
 radio.onReceivedNumber(function (receivedNumber) {
     item = receivedNumber
 })
-let item = 0
-radio.setGroup(44)
-basic.showIcon(IconNames.Happy)
-servos.P0.setRange(0, 360)
-basic.forever(function () {
+function remoteControl () {
     if (item == 1) {
         pins.servoWritePin(AnalogPin.P1, 0)
         pins.servoWritePin(AnalogPin.P2, 180)
@@ -18,4 +14,29 @@ basic.forever(function () {
     } else if (item == 2) {
         kitronik_servo_lite.stop()
     }
+}
+function lineFollow () {
+    rightDetector = pins.digitalReadPin(DigitalPin.P15)
+    leftDetector = pins.digitalReadPin(DigitalPin.P16)
+    if (leftDetector == 0 && rightDetector == 0) {
+        kitronik_servo_lite.stop()
+    } else if (leftDetector == 1 && rightDetector == 0) {
+        kitronik_servo_lite.right()
+    } else if (leftDetector == 0 && rightDetector == 1) {
+        kitronik_servo_lite.left()
+    } else if (leftDetector == 1 && rightDetector == 1) {
+        kitronik_servo_lite.forward()
+    }
+}
+let leftDetector = 0
+let rightDetector = 0
+let item = 0
+radio.setGroup(44)
+basic.showIcon(IconNames.Happy)
+servos.P0.setRange(0, 360)
+pins.setPull(DigitalPin.P15, PinPullMode.PullUp)
+pins.setPull(DigitalPin.P16, PinPullMode.PullUp)
+basic.forever(function () {
+    remoteControl()
+    lineFollow()
 })
