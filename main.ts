32c4522c6051974117f3caea04e9a3970f3d1843
fiologica <39060769+fiologica@users.noreply.github.com>
2023-01-12 18:31:47 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,24 +1,8 @@
 enum RadioMessage {
     message1 = 49434
 }
-function headLights () {
-    let strip: neopixel.Strip = null
-    headlights = 255 - input.lightLevel() * 3
-    if (input.lightLevel() < 85) {
-        strip.showColor(neopixel.colors(NeoPixelColors.White))
-        while (headlights < 50) {
-            headlights += 1
-        }
-        strip.setBrightness(headlights)
-        strip.show()
-    } else if (input.lightLevel() >= 95) {
-        strip.clear()
-        strip.show()
-    }
-}
 let leftDetector = 0
 let rightDetector = 0
-let headlights = 0
 basic.showIcon(IconNames.Happy)
 pins.setPull(DigitalPin.P15, PinPullMode.PullUp)
 pins.setPull(DigitalPin.P16, PinPullMode.PullUp)
