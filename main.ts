26e1a503f3635c36490d13104d8d54ba35c9eae4
fiologica <39060769+fiologica@users.noreply.github.com>
2023-01-12 04:11:39 +0000

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,29 +4,11 @@ enum RadioMessage {
 radio.onReceivedNumber(function (receivedNumber) {
     item = receivedNumber
 })
-function pixelHeadlights () {
-    headlights = 5 - input.lightLevel() * 3
-    if (input.lightLevel() < 85) {
-        pixelArray.showColor(neopixel.colors(NeoPixelColors.White))
-        while (headlights < 5) {
-            headlights += 1
-        }
-        pixelArray.setBrightness(headlights)
-        pixelArray.show()
-    } else if (input.lightLevel() >= 95) {
-        pixelArray.clear()
-        pixelArray.show()
-    }
-}
-let headlights = 0
 let item = 0
-let pixelArray: neopixel.Strip = null
 radio.setGroup(44)
 basic.showIcon(IconNames.Happy)
 servos.P0.setRange(0, 360)
-pixelArray = neopixel.create(DigitalPin.P0, 5, NeoPixelMode.RGB)
 basic.forever(function () {
-    pixelHeadlights()
     if (item == 1) {
         pins.servoWritePin(AnalogPin.P1, 0)
         pins.servoWritePin(AnalogPin.P2, 180)
